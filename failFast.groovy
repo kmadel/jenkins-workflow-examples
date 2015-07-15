31f4384b06fe96031dbd9b24137fae3779880082
Kurt Madel <kmadel@mac.com>
2015-07-15 07:36:53 -0700

    Update failFast.groovy

--- failFast.groovy
+++ failFast.groovy
@@ -2,7 +2,7 @@ node {
   try {
     parallel(
       b: { error 'died' },
-      // make sure this branch takes longer than a
+      // make sure this branch takes longer than b
       a: { sleep 25; writeFile text: '', file: 'a.done' },
       failFast: true
     )
