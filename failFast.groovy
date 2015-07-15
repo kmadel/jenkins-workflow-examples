07d2e63e441eb809550c97abddc3e09b0f3ca25f
Kurt Madel <kmadel@mac.com>
2015-07-15 07:53:07 -0700

    Update failFast.groovy

--- failFast.groovy
+++ failFast.groovy
@@ -1,12 +1,16 @@
+stage 'parallel with failFast'
+def error;
 node {
   try {
     parallel(
       b: { error 'died' },
       // make sure this branch takes longer than b
-      a: { sleep 25; writeFile text: '', file: 'a.done' },
+      a: { sleep 35; writeFile text: '', file: 'a.done' },
       failFast: true
     )
-  } catch (ParallelStepException e) {
-    echo e.toString()
+  } catch (e) {
+    error = e.toString()
   }
 }
+stage 'fail with error'
+echo "error: ${error}"
